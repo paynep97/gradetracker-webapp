2364cc79ecb89472630b8c52404021505dca2718
paynep97 <paynep97@gmail.com>
2021-11-18 11:49:02 -0600

    Added some starting components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomePageComponent } from './home-page/home-page.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeDashComponent } from './home-dash/home-dash.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    NavBarComponent,
+    HomeDashComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
     RouterModule.forRoot([
-      {path: 'home', component: HomePageComponent},
+      {path: 'home', component: HomeDashComponent},
 
     ]),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
